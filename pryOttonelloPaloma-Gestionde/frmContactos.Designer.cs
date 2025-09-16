a7a43a58c7bb74b59ae80e81182ef4c46fea2e10
palomillaaaaa <palomaottonello@gmail.com>
2025-09-16 13:31:22 -0300

    Se me borro lol.

---
+++ pryOttonelloPaloma-Gestionde/frmContactos.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace pryOttonelloPaloma_Gestionde
+{
+    partial class frmContactos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMostrar = new System.Windows.Forms.Label();
+            this.btmAtras = new System.Windows.Forms.Button();
+            this.btmSiguiente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblMostrar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrar.Location = new System.Drawing.Point(0, 0);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(632, 267);
+            this.lblMostrar.TabIndex = 0;
+            this.lblMostrar.Text = "-";
+            this.lblMostrar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMostrar.Click += new System.EventHandler(this.lblMostrar_Click);
+            // 
+            // btmAtras
+            // 
+            this.btmAtras.Location = new System.Drawing.Point(12, 283);
+            this.btmAtras.Name = "btmAtras";
+            this.btmAtras.Size = new System.Drawing.Size(261, 80);
+            this.btmAtras.TabIndex = 1;
+            this.btmAtras.Text = "Atrás";
+            this.btmAtras.UseVisualStyleBackColor = true;
+            this.btmAtras.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btmSiguiente
+            // 
+            this.btmSiguiente.Location = new System.Drawing.Point(371, 283);
+            this.btmSiguiente.Name = "btmSiguiente";
+            this.btmSiguiente.Size = new System.Drawing.Size(261, 80);
+            this.btmSiguiente.TabIndex = 2;
+            this.btmSiguiente.Text = "Siguiente";
+            this.btmSiguiente.UseVisualStyleBackColor = true;
+            this.btmSiguiente.Click += new System.EventHandler(this.btmSiguiente_Click);
+            // 
+            // frmContactos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btmSiguiente);
+            this.Controls.Add(this.btmAtras);
+            this.Controls.Add(this.lblMostrar);
+            this.Name = "frmContactos";
+            this.Text = "frmContactos";
+            this.Load += new System.EventHandler(this.frmContactos_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMostrar;
+        private System.Windows.Forms.Button btmAtras;
+        private System.Windows.Forms.Button btmSiguiente;
+    }
+}
---
+++ pryOttonelloPaloma-Gestionde/frmContactos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryOttonelloPaloma_Gestionde
+{
+    public partial class frmContactos : Form
+    {
+        public frmContactos()
+        {
+            InitializeComponent();
+        }
+
+        //D eclaración de Variables
+
+        string[] VecNombre = new string[3];
+        int indice = 0;
+        private void lblMostrar_Click(object sender, EventArgs e)
+        {
+            VecNombre[0] = "Matias";
+            VecNombre[1] = "Iñaki";
+            VecNombre[2] = "Mateo";
+
+            lblMostrar.Text = VecNombre[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            indice++;
+            lblMostrar.Text = VecNombre[indice];
+        }
+
+        private void frmContactos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btmSiguiente_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
